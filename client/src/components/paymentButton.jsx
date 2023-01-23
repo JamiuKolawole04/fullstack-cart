720e011810ee8732e6f92d8a39ea1552c4bed12d
JamiuKolawole04 <Jakko4real@gmail.com>
2023-01-23 02:04:00 +0100

    feat: cart checkout with stripe done

--- client/src/components/paymentButton.jsx
+++ client/src/components/paymentButton.jsx
@@ -1,8 +1,25 @@
+import { useSelector } from "react-redux";
+
 import axios from "../utils/axios";
 
 export const PaymentButton = ({ cartItems }) => {
+  const user = useSelector((state) => state.auth);
+
   const handleCheckout = () => {
-    console.log(cartItems);
+    axios({
+      method: "POST",
+      url: `/create-checkout-session`,
+      data: {
+        cartItems,
+        userId: user._id,
+      },
+    })
+      .then((res) => {
+        if (res.data.url) {
+          window.location.assign(res.data.url);
+        }
+      })
+      .catch((err) => console.log(err));
   };
 
   return <button onClick={() => handleCheckout()}>Check Out</button>;
