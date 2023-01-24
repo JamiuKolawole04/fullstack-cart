d1140530221077cb80b6f65d594892b8af983f00
JamiuKolawole04 <Jakko4real@gmail.com>
2023-01-24 07:20:36 +0100

    feat: adding authorization by admin to create product route

---
+++ api/middleware/auth.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  const token = req.header("x-auth-token");
+  if (!token) {
+    return res.status(401).json({
+      succss: false,
+      message: "Access denied.please log in...",
+    });
+  }
+
+  try {
+    const secretKey = process.env.ACCESS_TOKEN_SECRET;
+
+    const user = jwt.verify(token, secretKey);
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(400).json({
+      success: true,
+      message: err,
+    });
+  }
+};
+
+const isAdmin = (req, res, next) => {
+  auth(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(401).json({
+        succss: false,
+        message: "Access denied.Not authorized...",
+      });
+    }
+  });
+};
+
+module.exports = { auth, isAdmin };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -11,7 +11,8 @@ router.post("/register", async (req, res) => {
     name: Joi.string().min(3).max(30).required(),
     email: Joi.string().min(3).max(120).required().email(),
     password: Joi.string().min(3).max(1024).required(),
-    isAdmin: Joi.boolean(),
+    // isAdmin: Joi.boolean().default((parent) => parent.isAdmin == true),
+    isAdmin: Joi.boolean().invalid(),
   });
 
   const { error } = schema.validate(req.body);
