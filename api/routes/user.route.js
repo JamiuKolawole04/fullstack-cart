61e8dc78b3059a2d4bcd5d68de3d86827299a677
JamiuKolawole04 <Jakko4real@gmail.com>
2023-01-25 07:46:06 +0100

    chore: adding user endpoint

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,28 @@
 const { Router } = require("express");
+const moment = require("moment");
 const router = Router();
 
 const { User } = require("../models/user.model");
 const { auth, isAdmin, isUser } = require("../middleware/auth");
 
+console.log(moment());
+console.log(
+  moment()
+    .year(moment().year() - 1)
+    .fromNow()
+);
+
 // GET USER STATS
-// router.get("/status", async (req, res) => {
-//     const previousMonth =
-// })
+router.get("/stats", async (req, res) => {
+  const previousMonth = moment()
+    .month(moment().month() - 1)
+    .set("date", 1)
+    .format("YYYY-MM-DD HH:mm:ss");
+
+  res.status(200).json({
+    success: true,
+    message: previousMonth,
+  });
+});
 
 module.exports = router;
