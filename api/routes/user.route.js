1590c105f259900026d973c31ada85b8ceda5190
JamiuKolawole04 <Jakko4real@gmail.com>
2023-01-25 08:01:25 +0100

    feat: stats endpint created

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -5,24 +5,42 @@ const router = Router();
 const { User } = require("../models/user.model");
 const { auth, isAdmin, isUser } = require("../middleware/auth");
 
-console.log(moment());
-console.log(
-  moment()
-    .year(moment().year() - 1)
-    .fromNow()
-);
-
 // GET USER STATS
-router.get("/stats", async (req, res) => {
+router.get("/stats", isAdmin, async (req, res) => {
   const previousMonth = moment()
     .month(moment().month() - 1)
     .set("date", 1)
     .format("YYYY-MM-DD HH:mm:ss");
 
-  res.status(200).json({
-    success: true,
-    message: previousMonth,
-  });
+  try {
+    const users = await User.aggregate([
+      {
+        $match: { createdAt: { $gte: new Date(previousMonth) } },
+      },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      success: true,
+      message: "stats fetched successfully",
+      users,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: true,
+      message: err,
+    });
+  }
 });
 
 module.exports = router;
