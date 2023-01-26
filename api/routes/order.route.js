d14df1d7457c26edea9d525590e817f9e512d2df
JamiuKolawole04 <Jakko4real@gmail.com>
2023-01-26 08:21:15 +0100

    chore: adding update order route

--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -139,4 +139,26 @@ router.get("/", isAdmin, async (req, res) => {
   }
 });
 
+router.put(":id", async (req, res) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      updatedOrder,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: true,
+      message: err,
+    });
+  }
+});
+
 module.exports = router;
